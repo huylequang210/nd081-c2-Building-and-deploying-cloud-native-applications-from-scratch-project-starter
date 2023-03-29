410376931c4d40ba21e0ed7e12b834f7ee6baed9
huylequang210 <huylequanq210@gmail.com>
2023-03-29 23:44:15 +0700

    add API

--- NeighborlyAPI/eventHubTrigger/__init__.py
+++ NeighborlyAPI/eventHubTrigger/__init__.py
@@ -3,22 +3,17 @@ import logging
 import azure.functions as func
 
 
-def main(event: func.EventGridEvent):
-	logging.info('Function triggered to process a message: ', event.get_body())
-    logging.info('EnqueuedTimeUtc =', event.enqueued_time)
-    logging.info('SequenceNumber =', event.sequence_number)
-    logging.info('Offset =', event.offset)
+def main(event: func.EventHubEvent):
+    decodedBodyEvent = event.get_body().decode()
+    logging.info(f'Function triggered to process a message: {decodedBodyEvent}')
+    logging.info(f'EnqueuedTimeUtc = {event.enqueued_time}')
+    logging.info(f'SequenceNumber = {event.sequence_number}')
+    logging.info(f'Offset = {event.offset}')
 
-    result = json.dumps({
-        'id': event.id,
-        'data': event.get_json(),
-        'topic': event.topic,
-        'subject': event.subject,
-        'event_type': event.event_type,
-    })
+    result = json.dumps(decodedBodyEvent)
 
 
-    logging.info('Python EventGrid trigger processed an event: %s', result)
+    logging.info(f'Python EventGrid trigger processed an event: {result}')
 
 
 
